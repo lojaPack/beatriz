f252178a0a222a511262db16b0ebbe487d77240a
lojaPack <103005266+lojaPack@users.noreply.github.com>
2022-04-06 01:18:59 -0300

    Add files via upload

---
+++ funcao/app.js
@@ -0,0 +1,42 @@
+var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+  });
+
+  function mimo(){
+      const memimo = confirm('Enviar mimo para Ana de R$ 15,00')
+      if(memimo == true){
+          alert('Você sera direcionado ao mercado pago')
+          location='https://mpago.la/33zmawn'
+      } else{
+          alert(':-( Nossa princesa só queria um mimo')
+      }
+  }
+
+  function insta(){
+      location='https://twitter.com/morenax01?t=Us9NnY_wa740puGPBMEwJA&s=09'
+  }
+
+  function telegram(){
+      location='https://t.me/Morenax01'
+  }
+
+  function whast(){
+    location='https://api.whatsapp.com/send?phone=+5521990600761&text=Venda%20do%20meus%20porn%C3%B4s'
+}
+
+function pacote(){
+    location='https://api.whatsapp.com/send?phone=+5521990600761&text=Venda%20do%20meus%20porn%C3%B4s'
+}
+
+function chve(){
+    alert('Este perfil trabalha somente com vendas de pack')
+}
+
+function compraChave(){
+    alert('Este perfil não possui conteudo pago')
+}
+
+function irLoja(){
+    location='https://lojapack.github.io/loja/'
+}
